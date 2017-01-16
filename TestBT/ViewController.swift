64f137b37debf2bb73cf4296adb921a6d2bca192
Jan Kase <jan.kase@me.com>
2017-01-16 11:28:16 +0100

    úprava s rx

--- TestBT/ViewController.swift
+++ TestBT/ViewController.swift
@@ -9,23 +9,80 @@
 import UIKit
 import CoreBluetooth
 import Dispatch
+import RxBluetoothKit
+import RxSwift
 
 class ViewController: UIViewController {
   
   override func viewDidLoad() {
     super.viewDidLoad()
-    _btManage = CBCentralManager(delegate: self,
-                                 queue: DispatchQueue(label: "BT"),
-                                 options: [CBCentralManagerOptionRestoreIdentifierKey: "eu.jkdev.bttestbtcentral"])
+//    _btManager = CBCentralManager(delegate: self,
+//                                  queue: DispatchQueue(label: "BT"),
+//                                  options: [CBCentralManagerOptionRestoreIdentifierKey: "eu.jkdev.bttestbtcentral"])
+    _mainDisposeBag = DisposeBag()
+    
+    let theOptions: [String: AnyObject]
+        = [CBCentralManagerOptionRestoreIdentifierKey: NSString(string: "eu.jkdev.bttestbtcentral")]
+    _rxManager = BluetoothManager(queue: .main,
+                                  options: theOptions)
+    _rxManager.rx_state.subscribe(onNext: { [weak self] (aState: BluetoothState) in
+          print("\(aState)")
+          guard let theSelf: ViewController = self else {
+            return
+          }
+          switch aState {
+            case .unknown:
+              print("state unknown")
+            case .resetting:
+              print("state resetting")
+            case .unsupported:
+              print("state unsupported")
+            case .unauthorized:
+              print("state unauthorized")
+            case .poweredOff:
+              print("state powered off")
+            case .poweredOn:
+              print("state powered on")
+//            theSelf._rxManager.scanForPeripherals(withServices: nil).subscribe(onNext: {
+//              guard let theName = $0.peripheral.name else {
+//                return
+//              }
+//              print("\(theName) - \($0.peripheral.identifier.uuidString)")
+//            })
+              let theDeviceString = "114CA7C9-789B-4C33-8C4A-5E87105EE36F"
+              theSelf._rxManager
+                  .retrievePeripherals(withIdentifiers: [UUID(uuidString: theDeviceString)!])
+                  .subscribe(onNext: { [weak self] (aPeripherals: [Peripheral]) in
+                    guard let theSelf: ViewController = self else {
+                      return
+                    }
+                    theSelf._rxPeripheral = aPeripherals.first!
+                    theSelf._rxManager.connect(theSelf._rxPeripheral).subscribe(onNext: { [weak self] (aPeripheral: Peripheral) in
+                          guard let theSelf: ViewController = self else {
+                            return
+                          }
+                          aPeripheral.rx_isConnected.subscribe(onNext: {
+                                print($0 ? "\(theDeviceString) connected" : "\(theDeviceString) disconnected")
+                              }).addDisposableTo(theSelf._mainDisposeBag)
+                        }).addDisposableTo(theSelf._mainDisposeBag)
+                  }).addDisposableTo(theSelf._mainDisposeBag)
+            
+          }
+        }).addDisposableTo(_mainDisposeBag)
   }
+  
+  //<CBPeripheral: 0x1700eec80, identifier = 0D3E9EA2-8EC8-47DA-A05E-5E2E2358A7FD, name = pLoFuAH1, state = disconnected>
   
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
   }
   
-  fileprivate var _btManage:      CBCentralManager! = nil
-  fileprivate var _boxPeripheral: CBPeripheral!     = nil
+  fileprivate var _btManager:      CBCentralManager! = nil
+  fileprivate var _boxPeripheral:  CBPeripheral!     = nil
+  fileprivate var _rxManager:      BluetoothManager! = nil
+  fileprivate var _mainDisposeBag: DisposeBag!       = nil
+  fileprivate var _rxPeripheral:   Peripheral!       = nil
   // pokud není uloženo tak vrací misuse api a nefunguje
   
 }
